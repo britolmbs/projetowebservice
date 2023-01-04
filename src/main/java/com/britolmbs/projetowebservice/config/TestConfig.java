ef0846f4dfe566f2cdf01176f97955c0c287f848
britolmbs <britolmbs@hotmail.com>
2023-01-04 10:11:01 -0300

    Enum OrderStatus

--- src/main/java/com/britolmbs/projetowebservice/config/TestConfig.java
+++ src/main/java/com/britolmbs/projetowebservice/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.britolmbs.projetowebservice.config;
 
 import com.britolmbs.projetowebservice.entities.Order;
 import com.britolmbs.projetowebservice.entities.User;
+import com.britolmbs.projetowebservice.entities.enums.OrderStatus;
 import com.britolmbs.projetowebservice.repositories.OrderRepository;
 import com.britolmbs.projetowebservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
       User u1 = new User(null,"Lucas Brito", "lucas@gmail.com", "973052650", "123456");
       User u2 = new User(null, "Nathalia Valle", "nathalia@gmail.com", "9999999", "1234567");
 
-        Order o1 = new Order(null, Instant.parse("2023-01-03T14:36:00Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2023-02-27T03:42:10Z"),u2);
-        Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2023-01-03T14:36:00Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2023-02-27T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
       userRepository.saveAll(Arrays.asList(u1,u2));
       orderRepository.saveAll(Arrays.asList(o1, o2, o3));
